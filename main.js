a01b4ca46dc4653c257085bd517cc0821b08d6e5
sda0503 <sda0503@naver.com>
2024-03-09 23:28:32 +0900

    feat - 데이터 저장 및 삭제 추가

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 let img_arr = ['Bucket01','Bucket02','Bucket03','Bucket04','Bucket05','Bucket06'];
+let index = 0;
+
+$(document).ready(function(){
+    for(let i=0; i<localStorage.length; i++){
+        const key = window.localStorage.key(i);
+        loadData = JSON.parse(localStorage.getItem(key));
+        let appendData = `
+        <div id="${key}" class="Bucket">
+            <img class="bg" src="./assets/images/${loadData.bg}.png" alt="버킷리스트BG">
+            <img class="close" src="./assets/images/Delete.png" alt="삭제">
+            <p>${loadData.saveText}</p>
+            <img class="done" src="./assets/images/Done.png" alt="버킷리스트완료">
+        </div>
+        `;
+        $('.Bucket_list').append(appendData);
+    }
+})
+
 $('.flex_input img').click(function(){
     var text = $('.flex_input input').val();
     const random_img = img_arr[Math.floor(Math.random() * img_arr.length)];
     let appendData = `
-    <div class="Bucket">
+    <div id="list_${index}" class="Bucket">
         <img class="bg" src="./assets/images/${random_img}.png" alt="버킷리스트BG">
         <img class="close" src="./assets/images/Delete.png" alt="삭제">
         <p>${text}</p>
         <img class="done" src="./assets/images/Done.png" alt="버킷리스트완료">
     </div>
     `;
+    
     if(text){
         $('.Bucket_list').append(appendData);
+        const list = {
+            saveText: text,
+            bg: random_img,
+            doen: false,
+        };
+        localStorage.setItem('list_'+index, JSON.stringify(list));
+        index++;
     }else{
         alert("텍스트를 입력하세요.");
     }
@@ -23,5 +49,8 @@ $(document).on('click', '.fine' ,function(){
     $(this).removeClass('fine');
 })
 $(document).on('click', '.close' ,function(){
+    let name = $(this).parent().attr('id');
     $(this).parent().remove();
+    localStorage.removeItem(name);
 })
+
